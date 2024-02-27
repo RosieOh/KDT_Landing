6c802f402c30daacc74c91b52826c84d4f3c9ba4
RosieOh <dhxogns920@gmail.com>
2024-02-27 13:18:16 +0900

    🏫fix : WebConfig와 동일한 내용으로 인해 중복 로직 방지 작업 수행 #2🏫

--- src/main/java/com/kdt/landing/global/config/ServletConfig.java
+++ src/main/java/com/kdt/landing/global/config/ServletConfig.java
@@ -13,12 +13,12 @@ public class ServletConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
-        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
-        registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/static/fonts/");
-        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/static/assets/");
-        registry.addResourceHandler("/clEditor/**").addResourceLocations("classpath:/static/clEditor/");
-        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/resources/js/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/resources/css/");
+//        registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/static/fonts/");
+//        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/static/assets/");
+//        registry.addResourceHandler("/clEditor/**").addResourceLocations("classpath:/static/clEditor/");
+//        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
 
     }
 
