f4c16c8eeb816a832b1479b1762cc8ddf17780b3
RosieOh <dhxogns920@gmail.com>
2024-02-29 14:51:00 +0900

    🏫chore : Repository 파일명 중복으로 인한 수정 #36🏫

---
+++ src/main/java/com/kdt/landing/domain/Sub_PM/repository/Sub_PMRepository.java
@@ -0,0 +1,10 @@
+package com.kdt.landing.domain.Sub_PM.repository;
+
+import com.kdt.landing.domain.Sub_PM.entity.Sub_PM;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Sub_PMRepository extends JpaRepository<Sub_PM, Long> {
+
+}
