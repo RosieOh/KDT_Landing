9ebcec7b918ff163699a6d592696d752e1153872
sendjin5 <139240093+sendjin5@users.noreply.github.com>
2024-03-11 17:23:15 +0900

    #27 (#59)
    * Main index 및 css 추가
    
    * 🏫feat : 신청모집 Entity 완 #14🏫
    
    * 🏫feat: 신청과목 Enum 추가  #14🏫
    
    * 🏫 design: apply 임시화면 테스팅 #14🏫
    
    * feat: 필수 설정요소 변경
    
    * !HOTFIX: jsp 및 thymeleaf 설정 오류에 관한 긴급수정 #19
    
    * 🏫design : index 페이지 추가 및 Apply list 프론트 추가 #24🏫
    
    * 🏫[FEAT] Merge #27🏫
    
    * Merge 위함 #27
    
    * Merge #27
    
    * 🏫{FEAT} 빅데이터 교육신청 폼 BE 제작 #14🏫
    
    * 🏫{FEAT} 풀스택 교육신청 폼 BE 제작 #14🏫
    
    * 🏫{FEAT} PM 교육신청 폼 BE 제작 #14🏫
    
    * 🏫[FIX] css 간단한 수정 #33🏫
    
    * 폴더 구조 변경으로 인한 Merge #27
    
    * Css 수정 전 Merge #27
    
    * [DESIGN] 프론트엔드 작업 세팅 #24
    
    * [DESIGN] 프론트엔드 작업 세팅#24
    
    * [DESIGN] 프론트엔드 작업 세팅#24~
    
    * Subject 한개로 통합 예정, 컬럼 subject 추가#14
    
    * [DESIGN] 프론트엔드 작업 세팅 #24
    
    * [DESIGN] 프론트엔드 작업 세팅 #24
    
    * [DESIGN] index, fullstack css 전체 디자인 중  #24
    
    * [DESIGN] 프론트엔드 작업 세팅 #24
    
    * #27

--- src/main/java/com/kdt/landing/domain/home/HomeController.java
+++ src/main/java/com/kdt/landing/domain/home/HomeController.java
@@ -21,7 +21,6 @@ public class HomeController {
 
 
     //jsp index
-    @GetMapping("/")
     public String home(Principal principal,Model model) {
         memberService.createAdminMember(); // 관리자 회원 생성 메서드 호출
         return "/index";
@@ -36,6 +35,14 @@ public class HomeController {
     public String fullStack(Model model) {
         return "sub/fullstack";
     }
+<<<<<<< HEAD:src/main/java/com/kdt/landing/global/Rest/HomeController.java
+    @GetMapping("/bigdata")
+    public String bigdata(Model model) {
+        return "sub/bigdata";
+    }
+    @GetMapping("/pm")
+    public String pm(Model model) {
+=======
 
     @GetMapping("/data")
     public String subBigData(Model model) {
@@ -44,6 +51,7 @@ public class HomeController {
 
     @GetMapping("/pm")
     public String subPm(Model model) {
+>>>>>>> 85b3abeceedf8dd274f9a110029fe667be9b1e37:src/main/java/com/kdt/landing/domain/home/HomeController.java
         return "sub/pm";
     }
 }
