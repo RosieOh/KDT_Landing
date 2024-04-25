ab1dfee2c56cd95663b45f31c5a4e4cb2b001d1d
sendjin5 <sendjin5@gmail.com>
2024-04-25 15:20:56 +0900

    [DESIGN] Header 권한에 따른 메뉴바 수정 #45

--- src/main/java/com/kdt/landing/domain/home/HomeController.java
+++ src/main/java/com/kdt/landing/domain/home/HomeController.java
@@ -1,16 +1,24 @@
 
 package com.kdt.landing.domain.home;
 
+import com.kdt.landing.domain.member.dto.MemberJoinDTO;
 import com.kdt.landing.domain.member.repository.MemberRepository;
 import com.kdt.landing.domain.member.service.MemberService;
+import com.kdt.landing.global.oauth2.domain.PrincipalDetails;
 import com.kdt.landing.global.oauth2.service.PrincipalDetailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.security.Principal;
+import java.time.LocalDateTime;
 
 
 @Slf4j
@@ -34,18 +42,12 @@ public class HomeController {
     }
 
     @GetMapping("/subSign_full")
-    public String subSign_full(Model model, Principal principal, PrincipalDetailService principalDetailService) {
-        log.info("principal --------" + principal);
-        log.info("principal.getName() --------" + principal.getName());
-        log.info("principalDetailService --------" + principalDetailService.toString());
-        return "main/sign/subSign_full";
-    }
+    public String subSign_full() { return "main/sign/subSign_full";  }
 
     @GetMapping("/subSign_pm")
-    public String subSign_pm(Model model) {
+    public String subSign_pm() {
         return "main/sign/subSign_pm";
     }
-
 
 
     @GetMapping("/java")
@@ -61,5 +63,31 @@ public class HomeController {
     @GetMapping("/pm")
     public String subPm(Model model) {
         return "sub/pm";
+    }
+
+    @GetMapping("login")
+    public String Login(Model model){
+        return "member/login";
+    }
+
+    @GetMapping("join")
+    public String joinForm(Model model) {
+        return "member/join";
+    }
+
+    @PostMapping("joinPro")
+    public String join(Model model, MemberJoinDTO memberJoinDTO) {
+        memberJoinDTO.setLoginAt(LocalDateTime.now());
+        memberService.memberInsert(memberJoinDTO);
+        model.addAttribute("msg", "천재IT교육센터에 오신 것을 환영합니다!");
+        model.addAttribute("url", "/");
+        return "member/alert";
+    }
+
+    @PostMapping("idCheckPro")
+    public ResponseEntity idCheck(@RequestBody MemberJoinDTO memberJoinDTO) throws Exception {
+        String email = memberJoinDTO.getEmail();
+        boolean result = memberService.idCheck(email);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
